0996acbfa71c047396fcaa96d3cf8e30d4d32cf2
Brent Timmermans <brenttimmermans7@gmail.com>
2023-12-21 17:44:27 +0100

    ✨ add basic image detail page

--- app/[category]/[id]/page.tsx
+++ app/[category]/[id]/page.tsx
@@ -1,5 +1,35 @@
+import exifr from 'exifr'
+import Image from 'next/image'
 import Link from 'next/link'
+import styles from './page.module.css'
 
-export default function CategoryDetailPage() {
-  return <Link href="/street">back</Link>
+import path from 'path'
+
+interface Props {
+  params: { category: string; id: string }
+}
+
+export default async function CategoryDetailPage({
+  params: { category, id },
+}: Props) {
+  const filePath = path.resolve('public/images', category, id)
+  const { ExifImageWidth, ExifImageHeight } = await exifr.parse(filePath, [
+    'ExifImageWidth',
+    'ExifImageHeight',
+  ])
+
+  return (
+    <>
+      <Link href={`/${category}`}>&lt; Back</Link>
+      <section className={styles.container}>
+        <Image
+          src={`/images/${category}/${id}`}
+          alt={id}
+          width={ExifImageWidth}
+          height={ExifImageHeight}
+          className={styles.image}
+        />
+      </section>
+    </>
+  )
 }
