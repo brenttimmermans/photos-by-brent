2e347b671aeb8224e1a38f58ab796010cfae943e
Brent Timmermans <brenttimmermans7@gmail.com>
2023-12-22 15:54:06 +0100

    ✨ implement Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
+import Link from 'next/link'
+import styles from './page.module.css'
+
 export default function Home() {
   return (
-    <>
-      <p>Welcome to my portfolio website!</p>
-      <p>
-        Use one of the links in the top right to have a look at some of my work.
-      </p>
-    </>
+    <div className={styles.grid}>
+      <Link href="/night" className={styles.night}>
+        <section>
+          <div className={styles.text}>Night</div>
+          <div className={styles.background} />
+        </section>
+      </Link>
+      {/* <Link href="/racing" className={styles.racing}> */}
+      <Link href="/" className={styles.racing} aria-disabled>
+        <section>
+          <div className={styles.text}>Racing</div>
+          <div className={styles.background} />
+        </section>
+      </Link>
+      <Link href="/street" className={styles.street}>
+        <section>
+          <div className={styles.text}>Street</div>
+          <div className={styles.background} />
+        </section>
+      </Link>
+      {/* <Link href="/film" className={styles.film}> */}
+      <Link href="/" className={styles.film} aria-disabled>
+        <section>
+          <div className={styles.text}>Film</div>
+          <div className={styles.background} />
+        </section>
+      </Link>
+    </div>
   )
 }
