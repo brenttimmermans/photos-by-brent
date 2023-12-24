e4be55e6d6f0e6ec4e0c56cc08da73b56927ea20
Brent Timmermans <brenttimmermans7@gmail.com>
2023-12-24 11:24:57 +0100

    ✨ redirect when category is unknown

--- app/[category]/layout.tsx
+++ app/[category]/layout.tsx
@@ -1,7 +1,20 @@
+import { Category } from '@/app/types'
+import { redirect } from 'next/navigation'
+
 interface Props {
   children: React.ReactNode
+  params: {
+    category: string
+  }
 }
 
-export default function CategoryLayout({ children }: Props) {
+export default function CategoryLayout({
+  children,
+  params: { category },
+}: Props) {
+  if (!(category in Category)) {
+    redirect('/')
+  }
+
   return <>{children}</>
 }
---
+++ app/types.ts
@@ -0,0 +1,5 @@
+export enum Category {
+  street = 'street',
+  racing = 'racing',
+  night = 'night',
+}
