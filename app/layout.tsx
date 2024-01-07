6bfe00caeb6ae4ec237a62d05aae188763aaf94b
Brent Timmermans <brenttimmermans7@gmail.com>
2024-01-07 10:51:24 +0100

    ✨ add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/app/Header/Header'
 import '@/app/globals.css'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 
@@ -24,6 +25,7 @@ export default function RootLayout({ children, modal }: Props) {
         <main>{children}</main>
         {modal}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
